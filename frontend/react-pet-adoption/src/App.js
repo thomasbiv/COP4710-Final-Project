513e3f03d94ca32dd01784b3572ae9ab00484d7e
Thomas Bivins <bivinsthomas@gmail.com>
2021-10-27 22:30:03 -0400

    Added page navigation using conditional rendering

--- frontend/react-pet-adoption/src/App.js
+++ frontend/react-pet-adoption/src/App.js
@@ -1,26 +1,35 @@
 import pets from './pets.PNG'
 import Header from './components/HeaderComponent'
 import Button from './components/Button'
-
 import './App.css';
-
-const custClick = () => {
-  console.log('Customer login button clicked.') //Will add a new page here - Used to distinguish customer view
-}
-
-const empClick = () => {
-  console.log('Employee login button clicked') //Will add a new page here - Used to distinguish employee view
-}
+import {useState} from 'react'
 
 function App() {
+  const [btnToggle, setBtnToggle] = useState(0);
   return (
     <div className="App">
-      <Header title = 'REACT Pet Adoption'/> {/*Title is a prop being passed to the Header/CustBtn/EmpBtn components for use*/}
-      <Button title = 'Customer Login' onClick = {custClick}/>
-      <Button title = 'Employee Login' onClick = {empClick}/>
-      <div className="logo-container">
-        <img src = {pets} className = "pet-logo" alt =""/>
-      </div>
+      {btnToggle === 0
+        ? <div>
+            <Header title = 'REACT Pet Adoption'/> 
+            <Button title = 'Customer Login' onClick = {() => setBtnToggle(1)}/> {/*Clicking this button will change webpage to 'Customer State'*/}
+            <Button title = 'Employee Login' onClick = {() => setBtnToggle(2)}/> {/*Clicking this button will change webpage to 'Employee State'*/}
+            <div className="logo-container">                                     {/*(Each state will display the associated database view for that profile type)*/}
+              <img src = {pets} className = "pet-logo" alt =""/>
+            </div>
+          </div>
+        : btnToggle === 1
+        ? <div>
+            <div>Welcome valued customer!</div>
+            <div>Customer database view is displayed here! (Available Pets (and their medical issues), Pets foster condition, etc.)</div>
+            <Button title = 'Back' onClick = {() => setBtnToggle(0)}/>
+          </div>
+        :
+          <div>
+            <div>Welcome valued employee!</div>
+            <div>Employee database view is displayed here! (Pets (and their medical issues), Employee Info, Customer Info, etc.)</div>
+            <Button title = 'Back' onClick = {() => setBtnToggle(0)}/>
+          </div>
+      } 
     </div>
   );
 }
