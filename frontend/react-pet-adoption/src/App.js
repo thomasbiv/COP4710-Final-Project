16026330d38e1a23a83ddce7d7b785a58499c489
Garrett Ford <ford.garrettf@gmail.com>
2021-11-21 13:10:21 -0500

    test branch

--- frontend/react-pet-adoption/src/App.js
+++ frontend/react-pet-adoption/src/App.js
@@ -2,12 +2,31 @@
 import Header from './components/HeaderComponent'
 import Button from './components/Button'
 import image from './images/pets.PNG'
-import './form.css';
-import './App.css';
+import axios from 'axios'
+import './form.css'
+import './App.css'
 import {useState} from 'react'
 
 function App() {
   const [btnToggle, setBtnToggle] = useState(0);
+  const [customerId, setCustomerId] = useState("")
+  const [password, setPassword] = useState("")
+  const [RegCustomerId, setCustId] = useState("")
+  const [firstName, setFirst] = useState("")
+  const [lastName, setLast] = useState("")
+  const [phone, setPhone] = useState()
+  const [email, setEmail] = useState("")
+  const [address, setAddress] = useState("")
+  const [regPassword, setRegPassword] = useState("")
+
+  const login = () => {
+    axios.post("http://localhost:4000/customerLogin", {customerId: customerId, password: password}).then(() => {alert("it works")})
+  }
+
+  const register = () => {
+    axios.post("http://localhost:4000/customerRegister", {RegCustomerId: RegCustomerId, firstName: firstName, lastName: lastName, phone: phone, email: email, address: address, regPassword: regPassword}).then(() => {alert("it works")})
+  }
+
   return (
     <div className="App">
       {btnToggle === 0
@@ -22,14 +41,48 @@ function App() {
         : btnToggle === 1
         ? <div>
             <div className = "form-box">
-            {
-            CustomerLoginForm()
-            }   
+            <form>
+            <label>Welcome Customer!</label>
+            <br/>
+            <input type="text" placeholder="Customer ID" onChange={(e) => {setCustomerId(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
+            <br/>
+            <button onClick={login} className = 'btn'> Login</button>
+            <button className = "btn" onClick = {() => setBtnToggle(3)}>Register</button>
+            </form> 
             </div>
             Customer database view is displayed here! (Available Pets (and their medical issues), Pets foster condition, etc.)
             <br/>
             <Button title = 'Back' onClick = {() => setBtnToggle(0)}/>
           </div>
+        : btnToggle === 3
+        ? <div>
+          <div className = "form-box">
+          <form>
+            <label>Welcome New Customer!</label>
+            <br/>
+            <input type="text" placeholder="Customer ID" onChange={(e) => {setCustId(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="First Name" onChange={(e) => {setFirst(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Last Name" onChange={(e) => {setLast(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Phone Number" onChange={(e) => {setPhone(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Address" onChange={(e) => {setAddress(e.target.value)}}/>
+            <br/>
+            <input type="text" placeholder="Password" onChange={(e) => {setRegPassword(e.target.value)}}/>
+            <br/>
+            <button onClick={register} className = 'btn'> Register</button>
+            </form> 
+        </div>
+        Customer database view is displayed here! (Available Pets (and their medical issues), Pets foster condition, etc.)
+        <br/>
+        <Button title = 'Back' onClick = {() => setBtnToggle(0)}/>
+      </div>
         :
           <div>
             <div className="form-box">
@@ -45,20 +98,7 @@ function App() {
   );
 }
 
-function CustomerLoginForm() {
 
-  return (
-    <form>
-      <label>Welcome Customer!</label>
-      <br/>
-      <input type="text" name="customerId" placeholder="Customer ID"/>
-      <br/>
-      <input type="text" name="customerPassword" placeholder="Password"/>
-      <br/>
-      <input type="submit" value="Submit" className = "btn"/>
-    </form>
-  )
-}
 
 function EmployeeLoginForm() {
 
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const {Client} = require('pg')
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = 4000
+
+const client  = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "password",
+    database: "animalShelter"
+})
+
+client.connect();
+
+app.get("/customerLogin", (req, res) => {
+    const customerId = req.body.customerId
+    const password = req.body.password
+    client.query("SELECT * FROM customer WHERE customerid = 123 AND password = password", [customerId, password],
+    (err, result) => {
+        console.log(err)
+        console.log(result.rows)
+        if(err) {
+            res.send({err: err});
+        } 
+        if (result){
+            res.send(result.rows)
+        } else {
+            res.send({message: "wrong id/password"})
+        }
+        
+    })
+    client.end;
+})
+
+app.get("/customerRegister", (req, res) => {
+    const customerId = req.body.regCustomerId
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+    const phone = req.body.phone
+    const address = req.body.address
+    const password = req.body.regPassword
+    client.query("INSERT INTO customer VALUES (?,?,?,?,?,?,?)", [customerId, firstName, lastName, email, phone, address, password],
+    (err, result) => {
+        console.log(err)
+    })
+    client.end;
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
